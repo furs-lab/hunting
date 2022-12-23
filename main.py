6f62070eb518e3f134a67bae8533275f63e78d92
fursenko <roman.fursenko@gmail.com>
2022-12-23 17:26:01 +0700

    add stats

--- main.py
+++ main.py
@@ -69,12 +69,12 @@ class Target(Player):
 
 
 class Game:
-    predator_dfi_max = 15
-    predator_dfi_min = 14
+    predator_dfi_max = 16
+    predator_dfi_min = 15
     predator_v_max = 1.31
     predator_v_min = 1.3
     predator_start_max = 50.1
-    predator_start_min = 50
+    predator_start_min = 50.0
     prey_start_x = 20
     prey_start_y = 20
     prey_dfi = 10
@@ -83,9 +83,10 @@ class Game:
     target_y = 80
 
     def __init__(self, net, n_steps=100, r_collision=0, Lx=100, Ly=100):
-        self.prey = Prey(self.prey_start_x, self.prey_start_y, maxdfi=np.pi / self.prey_dfi, v=self.prey_v, net=net)
+        self.prey = Prey(self.prey_start_x, self.prey_start_y, fi=np.pi/4,
+                         maxdfi=np.pi / self.prey_dfi, v=self.prey_v, net=net)
         self.predator = Predator(uniform(self.predator_start_min, self.predator_start_max),
-                                 uniform(self.predator_start_min, self.predator_start_max),
+                                 0*uniform(self.predator_start_min, self.predator_start_max),
                            maxdfi=np.pi / randrange(self.predator_dfi_min, self.predator_dfi_max),
                            v=uniform(self.predator_v_min, self.predator_v_max))
         self.target = Target(self.target_x, self.target_y)
@@ -110,9 +111,9 @@ class Game:
     def calculate_score(self, prey, predator, target):
         prey.score += 1
         if self.is_collision(prey, predator):
-            prey.score -= 1000
-        if self.is_outside(prey):
             prey.score -= 10000
+        if self.is_outside(prey):
+            prey.score -= 1000
         if self.is_collision(prey, target):
             prey.score += 10000
         return prey.score
@@ -134,7 +135,7 @@ class Game:
             if self.is_collision(self.prey, self.target):
                 game_over = True
             step += 1
-        self.prey.score -= 1000 * int(self.prey.distance_to(self.target))
+        self.prey.score -= 150 * int(self.prey.distance_to(self.target))
         return self.prey.score
 
 
@@ -143,7 +144,7 @@ def run_evolution(genomes, config):
         genome.fitness = 0.0
         net = neat.nn.FeedForwardNetwork.create(genome, config)
 
-        game = Game(net, n_steps=500, r_collision=1)
+        game = Game(net, n_steps=300, r_collision=1)
         genome.fitness = game.run()
 
 
@@ -171,18 +172,27 @@ def main():
                                 neat.DefaultStagnation, config_path)
     # init NEAT
     p = neat.Population(config)
+    # Add a stdout reporter to show progress in the terminal.
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
     # run NEAT
-    winner = p.run(run_evolution, 100)
+    winner = p.run(run_evolution, 500)
     winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
 
-    for i in range(0, 5):
-        game = Game(winner_net, n_steps=500, r_collision=1)
+    ts = np.arange(0, len(stats.get_fitness_median()), 1).tolist()
+    fig, ax = plt.subplots()
+    ax.plot(ts, stats.get_fitness_median())
+    plt.show()
+
+    for i in range(0, 1):
+        game = Game(winner_net, n_steps=300, r_collision=1)
         score = game.run()
         print(f"{score=}, {game.n_collisions=}")
         p1 = pg.plot()
         show_plot(game.prey, game.predator, game.target, p1)
 
-    # show_plot(game.prey, game.predator, game.target)
+
 
 
 if __name__ == '__main__':
